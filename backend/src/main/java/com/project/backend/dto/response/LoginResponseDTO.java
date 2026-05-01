d25c786b5d9ed3103ab335f2f5919abd4bbbc64b
thomaspham252 <22130143@st.hcmuaf.edu.vn>
2026-05-01 18:26:56 +0700

    khởi tạo thư mục BE

---
+++ backend/src/main/java/com/project/backend/dto/response/LoginResponseDTO.java
@@ -0,0 +1,21 @@
+package com.project.backend.dto.response;
+
+public class LoginResponseDTO {
+
+    private String token;
+
+    public LoginResponseDTO() {
+    }
+
+    public LoginResponseDTO(String token) {
+        this.token = token;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+}
